af3264b32aaa1e705d80bb4ef0cb4c57ec1f12e4
Shaswat Prabhat <spd1238@gmail.com>
2020-12-26 10:16:58 +0530

    FrenchDeck for Idiomatic Python.

---
+++ PythonicDataModel/FrenchDeck.py
@@ -0,0 +1,25 @@
+import collections
+from random import choice
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('AJKQ')
+    suits = 'clubs diamonds hearts spades'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for rank in self.ranks for suit in self.suits]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+if __name__ == '__main__':
+    deck = FrenchDeck()
+    print(choice(deck))
+    for card in reversed(deck):
+        print(card)
